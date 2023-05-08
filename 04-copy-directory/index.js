4730e1d3463e12a0b012cca0704c3911d1d3743f
unknown <home_dv@mail.ru>
2023-05-08 22:54:34 +0300

    fix: add check for missing directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,18 +1,26 @@
 const fs = require('node:fs/promises');
 const path = require('path');
-const { truncate } = require('fs');
+const { access } = require('fs');
 
 const dirName = 'files';
 const newDirPath = path.join(__dirname, `${dirName}_copy`);
 
-
 const copyDir = async () => {
-  truncate(newDirPath, (err) => {console.log(err)});
-  await fs.mkdir(newDirPath, {recursive: true});
-  const files = await fs.readdir(path.join(__dirname, dirName), {withFileTypes: true});
-  files.forEach(async (file) => {
-    const fileName = `${file.name.split('.')[0]}${path.extname(file.name)}`;
-    await fs.copyFile(path.join(__dirname, dirName, file.name), `${newDirPath}/${fileName}`);
+  access(newDirPath, async (err) => {
+    if(err) {
+      fs.mkdir(newDirPath, {recursive: true});
+    }
+    else {
+      const files = await fs.readdir(newDirPath, () => {});
+      files.forEach(async (file) => {
+        fs.unlink(path.join(newDirPath, file));
+      })
+    }
+    const files = await fs.readdir(path.join(__dirname, dirName), {withFileTypes: true});
+    files.forEach(async (file) => {
+      const fileName = `${file.name.split('.')[0]}${path.extname(file.name)}`;
+      await fs.copyFile(path.join(__dirname, dirName, file.name), `${newDirPath}/${fileName}`);
+    })
   })
 }
 
